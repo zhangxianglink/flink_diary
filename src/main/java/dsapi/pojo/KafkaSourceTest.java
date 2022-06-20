90d0d8f68bd27f3de8d30cacd09611350f05684e
zhangxiang <xiangzhang@x2era.com>
2022-06-20 17:55:04 +0800

    fea: kafka 消费成功。https://nightlies.apache.org/flink/flink-docs-release-1.13/zh/docs/connectors/datastream/kafka/

---
+++ src/main/java/dsapi/pojo/KafkaSourceTest.java
@@ -0,0 +1,32 @@
+package dsapi.pojo;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
+
+/**
+ * socket无界流测试
+ * @author xiangzhang
+ * @since 2022-06-18 20:23
+ */
+public class KafkaSourceTest {
+    public static void main(String[] args) throws Exception {
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","124.222.222.46:9092");
+        properties.setProperty("group.id","win_test");
+        final FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>("flink_test1", new SimpleStringSchema(), properties);
+        kafkaConsumer.setStartFromEarliest();
+        final DataStreamSource<String> stream
+                = env.addSource(kafkaConsumer);
+
+        stream.print("kafka");
+        env.execute();
+    }
+}
