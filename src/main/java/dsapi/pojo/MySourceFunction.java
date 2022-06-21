317bc940a52e5dca0c920fd2420bba3750942f31
zhangxiang <xiangzhang@x2era.com>
2022-06-21 14:29:56 +0800

    fea: 1

---
+++ src/main/java/dsapi/pojo/MySourceFunction.java
@@ -0,0 +1,8 @@
+package dsapi.pojo;
+
+/**
+ * @author xiangzhang
+ * @since 2022-06-21 14:27
+ */
+public class MySourceFunction {
+}
---
+++ src/main/java/dsapi/pojo/MySourceTest.java
@@ -0,0 +1,15 @@
+package dsapi.pojo;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author xiangzhang
+ * @since 2022-06-21 14:26
+ */
+public class MySourceTest {
+
+    public static void main(String[] args) {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    }
+}
